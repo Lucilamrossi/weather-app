0fb3adbb13a57e0f21796cbbba7f2a9c2e11d386
Lucilamrossi <lucilamrossi@gmail.com>
2021-07-08 02:32:19 -0300

    run build

---
+++ build/precache-manifest.a23beec761ef4d77f10286831c186e34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "027826a85e32ab0929af3aaa0d71ecf8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0ee51bf90d247ba09de",
+    "url": "/static/css/main.a7dab485.chunk.css"
+  },
+  {
+    "revision": "9832fc5ec999c64fc9f0",
+    "url": "/static/js/2.95bb9362.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.95bb9362.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0ee51bf90d247ba09de",
+    "url": "/static/js/main.0812e4a0.chunk.js"
+  },
+  {
+    "revision": "16424b2c5fc1ee8e9e17",
+    "url": "/static/js/runtime-main.ca0f29c7.js"
+  },
+  {
+    "revision": "30d3e9c36b657dc3ab290ea88eca2d6a",
+    "url": "/static/media/sky4.30d3e9c3.jpg"
+  }
+]);
